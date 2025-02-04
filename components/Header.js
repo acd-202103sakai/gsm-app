f2c47fb4e16dd5bf62fb41f3b7e58476da5bf3d4
sakai0997 <sakai.tadafumi0997@inspace.co.jp>
2025-02-04 12:05:01 +0900

    初回コミット（nextjsプロジェクト作成）

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css"
+
+const Header = () => (
+    <div className={styles.header}>
+        <b className={styles.header_name}>システム名</b>
+    </div>
+);
+
+export default Header;
